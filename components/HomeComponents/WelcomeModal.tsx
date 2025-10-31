32da2f09c40e8d8bc951850d04b36b9fb1e35f80
Akem-Ben <akemini.ndaobong@gmail.com>
2025-10-31 14:49:09 +0100

    Feat: Welcome modal writeup correction and addition of AKSG logo and Arise logo to the welcome modal

--- components/HomeComponents/WelcomeModal.tsx
+++ components/HomeComponents/WelcomeModal.tsx
@@ -5,6 +5,7 @@ import {
   DialogTitle,
 } from "./HomeDailogue";
 import Button from "@/components/HomeComponents/HomeButton";
+import Image from "next/image";
 
 interface WelcomeModalProps {
   open: boolean;
@@ -16,19 +17,43 @@ const WelcomeModal = ({ open, onContinue, onClose }: WelcomeModalProps) => {
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogHeader className="space-y-6">
-        <DialogTitle className="text-2xl sm:text-3xl text-center md:text-4xl text-[#2a2523] font-bold">
-          Welcome to Akwa Ibom State Hotel and Tourism Board Portal
-        </DialogTitle>
-        <DialogDescription className="text-base text-center sm:text-lg md:text-xl text-[#78716e] leading-relaxed text-muted-foreground">
+        <section className="flex justify-between relative w-full h-16 md:h-24 mb-4">
+          <div>
+            <Image
+              src="/akwa-ibom-logo-main.png"
+              alt="Celebration"
+              width={100}
+              height={100}
+              style={{ objectFit: "cover" }}
+              priority
+            />
+          </div>
+          <DialogTitle className="text-xl sm:text-2xl text-center md:text-3xl text-[#2a2523] font-bold">
+            Welcome to Akwa Ibom State Hotel and Tourism Board Portal
+          </DialogTitle>
+          <div>
+            <Image
+              src="/arise-logo-main.png"
+              alt="Celebration"
+              width={100}
+              height={100}
+              style={{ objectFit: "cover" }}
+              priority
+            />
+          </div>
+        </section>
+        <DialogDescription className="text-base text-center sm:text-md md:text-lg text-[#78716e] leading-relaxed text-muted-foreground">
           As part of its mandate to register and regulate all hospitality
           establishments, the Akwa Ibom State Hotel and Tourism Board seeks to
-          capture all information of these establishments. It is mandatory that
-          all hospitality establishments log on to this portal and register.
+          capture all information of these establishments. <br /> It is
+          mandatory that all hospitality establishments operating a business in
+          Akwa Ibom State log on to this portal and register their hospitality
+          establishment/business.
         </DialogDescription>
       </DialogHeader>
 
       <div className="flex justify-center md:justify-end pt-4">
-        <Button onClick={onContinue} background="#e77818">
+        <Button onClick={onContinue} background="#e77818" height="50px">
           Continue to Registration
         </Button>
       </div>
