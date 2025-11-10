03614d2a0b1e332daf069af3f4df0c46c794946f
Akem-Ben <akemini.ndaobong@gmail.com>
2025-11-10 10:24:48 +0100

    Fix: Trigger Deployment

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,24 +10,32 @@ const NotFound = () => {
 
   useEffect(() => {
     if (pathname) {
-      console.error("404 Error: User attempted to access non-existent route:", pathname);
+      console.error(
+        "404 Error: User attempted to access non-existent route:",
+        pathname
+      );
     }
   }, [pathname]);
 
   return (
     <div>
-        <Header />
-        <div className="flex min-h-[80vh] items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold text-gray-600">404</h1>
-        <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
-        <p className="mb-8 text-gray-600">Could not find the requested resource</p>
-        <Link href="/" className="text-blue-500 underline hover:text-blue-700">
-          Return to Home
-        </Link>
-      </div>
+      <Header />
+      <div className="flex min-h-[80vh] items-center justify-center bg-gray-100">
+        <div className="text-center">
+          <h1 className="mb-4 text-4xl font-bold text-gray-600">404</h1>
+          <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
+          <p className="mb-8 text-gray-600">
+            Could not find the requested resource
+          </p>
+          <Link
+            href="/"
+            className="text-blue-500 underline hover:text-blue-700"
+          >
+            Return to Home
+          </Link>
         </div>
-        <Footer />
+      </div>
+      <Footer />
     </div>
   );
 };
